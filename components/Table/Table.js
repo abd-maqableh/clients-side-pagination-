3c168eb5029f61a9995fdacc5405bd654378e57c
abd-maqableh <abd.maqableh@gmail.com>
2022-05-27 18:15:49 +0300

    fix: fix date filter

--- components/Table/Table.js
+++ components/Table/Table.js
@@ -6,42 +6,19 @@ import css from "./Table.module.scss";
 import isObjectValueEmpty from "../../utils/isEmptyObject";
 import TableHead from "./TableHead";
 import TableBody from "./TableBody";
+import { filterDate, handleSorting } from "../../utils/appUtils";
 const Table = ({ data }) => {
   const formFilter = useSelector((state) => state.FormData.formFilter);
   const [tableData, setTableData] = useState();
 
   useEffect(() => {
     if (!isEmpty(data) && !isObjectValueEmpty(formFilter)) {
-      const filterTableData = data?.filter(
-        (item) =>
-          item.applicationType === formFilter.applicationType ||
-          item.actionType === formFilter.actionType ||
-          item.applicationId === formFilter.applicationID ||
-          item.userId == formFilter.employeeID ||
-          item.creationTimestamp?.split(" ")[0] <= formFilter.fromDate ||
-          item.creationTimestamp?.split(" ")[0] >= formFilter.toDate
-      );
+      const filterTableData = filterDate(data, formFilter);
       setTableData(!isEmpty(filterTableData) ? filterTableData : []);
     } else {
       setTableData(data);
     }
   }, [data, formFilter]);
-
-  const handleSorting = (sortField, sortOrder) => {
-    if (sortField) {
-      const sorted = [...tableData].sort((a, b) => {
-        if (a[sortField] === null) return 1;
-        if (b[sortField] === null) return -1;
-        if (a[sortField] === null && b[sortField] === null) return 0;
-        return (
-          a[sortField].toString().localeCompare(b[sortField].toString(), "en", {
-            numeric: true,
-          }) * (sortOrder === "asc" ? 1 : -1)
-        );
-      });
-      setTableData(sorted);
-    }
-  };
 
   return isEmpty(tableData) ? (
     <div>
@@ -51,7 +28,11 @@ const Table = ({ data }) => {
   ) : (
     <table className={css.table}>
       <tbody>
-        <TableHead handleSorting={handleSorting} />
+        <TableHead
+          handleSorting={(sortField, sortOrder) =>
+            setTableData(handleSorting(sortField, sortOrder, tableData))
+          }
+        />
         <TableBody tableData={tableData} />
       </tbody>
     </table>
